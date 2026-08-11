d871c252e6b8fdb829a9e6fd5d67e134d23648d5
RipsySais <cysseahmed@gmail.com>
2026-08-11 14:01:19 +0200

    Fix 4-frequency.py xlim and xticks

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -14,4 +14,6 @@ def frequency():
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
+    plt.xlim(0, 100)
+    plt.xticks(range(0, 101, 10))
     plt.show()
