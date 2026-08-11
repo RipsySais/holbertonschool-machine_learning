ee2647067986d9fc9efb1c0db507320924ad8a3b
RipsySais <cysseahmed@gmail.com>
2026-08-12 00:11:29 +0200

    Add 6-bars.py

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Module that plots a stacked bar graph of fruit quantities"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bars():
+    """Plots a stacked bar graph of fruit per person"""
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    plt.figure(figsize=(6.4, 4.8))
+
+    people = ['Farrah', 'Fred', 'Felicia']
+    x = np.arange(3)
+
+    plt.bar(x, fruit[0], width=0.5, color='red', label='apples')
+    plt.bar(x, fruit[1], width=0.5, bottom=fruit[0],
+            color='yellow', label='bananas')
+    plt.bar(x, fruit[2], width=0.5, bottom=fruit[0] + fruit[1],
+            color='#ff8000', label='oranges')
+    plt.bar(x, fruit[3], width=0.5,
+            bottom=fruit[0] + fruit[1] + fruit[2],
+            color='#ffe5b4', label='peaches')
+
+    plt.xticks(x, people)
+    plt.ylabel('Quantity of Fruit')
+    plt.ylim(0, 80)
+    plt.yticks(range(0, 81, 10))
+    plt.title('Number of Fruit per Person')
+    plt.legend()
+    plt.show()
