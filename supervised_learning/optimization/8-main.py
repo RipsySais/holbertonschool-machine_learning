4ae661c5f4924a3bd783f4503d7bdc39ffb5a27e
RipsySais <cysseahmed@gmail.com>
2026-09-11 03:08:08 +0200

    Add 8-RMSProp.py

---
+++ supervised_learning/optimization/8-main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import random
+import os
+
+SEED = 0
+
+os.environ['PYTHONHASHSEED'] = str(SEED)
+os.environ['TF_ENABLE_ONEDNN_OPTS']= '0'
+random.seed(SEED)
+np.random.seed(SEED)
+tf.random.set_seed(SEED)
+
+create_RMSProp_op = __import__('8-RMSProp').create_RMSProp_op
+
+def one_hot(Y, classes):
+    """convert an array to a one-hot matrix"""
+    one_hot = np.zeros((Y.shape[0], classes))
+    one_hot[np.arange(Y.shape[0]), Y] = 1
+    return one_hot
+
+lib = np.load('../data/MNIST.npz')
+X_3D = lib['X_train']
+Y = lib['Y_train']
+X = X_3D.reshape((X_3D.shape[0], -1))
+Y_oh=one_hot(Y,10)
+
+model = tf.keras.models.load_model('model.h5', compile=False)
+
+optimizer=create_RMSProp_op(0.001, 0.9, 1e-07)
+
+@tf.function
+def train_step(inputs, labels):
+    with tf.GradientTape() as tape:
+        predictions = model(inputs)
+        loss = tf.reduce_mean(tf.keras.losses.CategoricalCrossentropy()(labels, predictions))
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    return loss
+
+total_iterations = 1000
+for iteration in range(total_iterations):
+
+    cost = train_step(X, Y_oh)
+
+    if (iteration + 1) % 100 == 0:
+        print(f'Cost after {iteration + 1} iterations: {cost}')
