3320a87ea99e0a7746fb765c017e8a8e8c28b444
RipsySais <cysseahmed@gmail.com>
2026-09-18 09:32:22 +0200

    Add 4-f1_score.py

---
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module that calculates the F1 score of a confusion matrix"""
+import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
+
+def f1_score(confusion):
+    """Calculates the F1 score of a confusion matrix
+
+    Args:
+        confusion: confusion numpy.ndarray of shape (classes, classes)
+            where rows are correct labels and columns are predicted
+    """
+    sens = sensitivity(confusion)
+    prec = precision(confusion)
+    return 2 * (prec * sens) / (prec + sens)
